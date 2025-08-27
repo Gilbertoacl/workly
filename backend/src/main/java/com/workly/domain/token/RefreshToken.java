81fd5ac8e17fa08fa8b962c78d7471a766e0f87b
Gilbertoacl <betoacl@hotmail.com>
2025-08-27 17:09:09 -0300

    Feat: adicionado a entidade representante do Refresh Token

---
+++ backend/src/main/java/com/workly/domain/token/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.workly.domain.token;
+
+import com.workly.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "expiry_date", nullable = false)
+    private Instant expiryDate;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant created_at = Instant.now();
+}
