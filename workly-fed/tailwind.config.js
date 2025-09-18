88c6a464b05594fb88a0180c7b00a5c9728e941f
Gilbertoacl <betoacl@hotmail.com>
2025-09-18 19:50:03 -0300

    feat: Adicionado a pagina com o formulário de login.

--- workly-fed/tailwind.config.js
+++ workly-fed/tailwind.config.js
@@ -7,9 +7,12 @@ export default {
       colors: {
         background: 'var(--color-background)',
         surface: 'var(--color-surface)',
+        surfaceAlt: 'var(--color-surface-alt)',
         textPrimary: 'var(--color-text-primary)',
         textSecondary: 'var(--color-text-secondary)',
         border: 'var(--color-border)',
+        btnPrimary:'var(--color-primary)',
+        btnPrimaryHover: 'var(--color-primary-hover)', 
         primary: {
           DEFAULT: 'var(--color-primary)',
           text: 'var(--color-primary-text)',
