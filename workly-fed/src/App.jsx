d8c64c2ada6b0c8129148b09bc8e62dd7ecd0d93
Gilbertoacl <betoacl@hotmail.com>
2025-09-19 11:31:54 -0300

    feat: Adicionado o redirecionamento para a pagina de login

--- workly-fed/src/App.jsx
+++ workly-fed/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
@@ -7,6 +7,7 @@ import RegisterPage from './pages/RegisterPage'
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/home" element={<MainPage />} />   
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
