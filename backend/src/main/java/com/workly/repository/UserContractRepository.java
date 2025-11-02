11eb5b94891f2418af459caedd49b7f0ed4ed6bb
Gilbertoacl <betoacl@hotmail.com>
2025-11-02 14:47:15 -0300

    feat(Repository): Adicionado no metodo de busca de vagas no repository

--- backend/src/main/java/com/workly/repository/UserContractRepository.java
+++ backend/src/main/java/com/workly/repository/UserContractRepository.java
@@ -1,8 +1,7 @@
 package com.workly.repository;
 
 
-import com.workly.dto.UserContractResponseDTO;
-import com.workly.entity.ScrapedJob;
+import com.workly.dto.user.UserContractResponseDTO;
 import com.workly.entity.User;
 import com.workly.entity.UserContract;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,12 +13,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserContractRepository extends JpaRepository<UserContract, Long> {
-    List<UserContract> findByUser(UserDetails user);
     boolean existsByUserAndJobLinkHash(User user, String jobLinkHash);
     Optional<UserContract> findByUserAndJobLinkHash(User user, String jobLinkHash);
 
     @Query(value = """
-        SELECT new com.workly.dto.UserContractResponseDTO(
+        SELECT new com.workly.dto.user.UserContractResponseDTO(
             us.status,
             sj.title,
             sj.description,
@@ -36,4 +34,23 @@ public interface UserContractRepository extends JpaRepository<UserContract, Long
         WHERE us.user.id = :userId
     """)
     List<UserContractResponseDTO> findJobsByUser(@Param("userId") Long userId);
+
+    @Query("""
+        SELECT new com.workly.dto.user.UserContractResponseDTO(
+            uc.status,
+            sj.title,
+            sj.description,
+            sj.linkHash,
+            sj.link,
+            sj.minBudget,
+            sj.maxBudget,
+            sj.scraped_at,
+            sj.skills,
+            sj.source
+        )
+        FROM UserContract uc
+        JOIN ScrapedJob sj ON uc.jobLinkHash = sj.linkHash
+        WHERE uc.user = :user AND uc.jobLinkHash = :linkHash
+    """)
+    Optional<UserContractResponseDTO> findContractDTOByUserAndLinkHash(@Param("user") User user, @Param("linkHash") String linkHash);
 }
