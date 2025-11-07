e5e6e8b7b449a74d95350ecea7c96896b2c08e02
Gilbertoacl <betoacl@hotmail.com>
2025-11-07 19:01:51 -0300

    feat(SecurityConfig) Adicionado o metodo PATCH na lista de verbos que o backend vai aceitar

--- backend/src/main/java/com/workly/config/SecurityConfig.java
+++ backend/src/main/java/com/workly/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("POST", "PUT", "DELETE", "GET", "OPTIONS"));
+        config.setAllowedMethods(List.of("POST", "PUT", "DELETE", "GET", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
