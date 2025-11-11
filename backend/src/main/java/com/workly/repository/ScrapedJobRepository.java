2fed16fbc5c6c10e22367e2d0b410e2a1bffd0b5
Gilbertoacl <betoacl@hotmail.com>
2025-11-11 16:19:02 -0300

    feat(Repository) Adicionado os metodos de buscas de vagas por titulo ou habilidade

--- backend/src/main/java/com/workly/repository/ScrapedJobRepository.java
+++ backend/src/main/java/com/workly/repository/ScrapedJobRepository.java
@@ -2,6 +2,23 @@ package com.workly.repository;
 
 import com.workly.entity.ScrapedJob;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ScrapedJobRepository extends JpaRepository<ScrapedJob, Long> {
+
+    @Query(value = """
+        SELECT * FROM scraped_jobs sj
+        WHERE sj.skills ~* ('(^|\\|)\\s*' || :query || '\\s*($|\\|)')
+    """, nativeQuery = true)
+    List<ScrapedJob> searchJobBySkill(@Param("query") String query);
+
+    @Query(value = """
+        SELECT * FROM scraped_jobs sj
+        WHERE sj.title ILIKE %:query%
+    """, nativeQuery = true)
+    List<ScrapedJob> searchJobByKeyword(@Param("query") String query);
+
 }
