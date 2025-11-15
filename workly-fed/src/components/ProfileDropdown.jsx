4b7730b65f4a68697dc2bc34855d05906f014c62
Gilbertoacl <betoacl@hotmail.com>
2025-11-15 18:03:22 -0300

    refacto (Profile) Mudança na exibição do menu de perfil

--- workly-fed/src/components/ProfileDropdown.jsx
+++ workly-fed/src/components/ProfileDropdown.jsx
@@ -1,8 +1,13 @@
+import useAuth from "@/hooks/useAuth";
 import { useEffect, useRef, useState } from "react";
+import { FaInfoCircle, FaRegUser } from "react-icons/fa";
+import { FaArrowRightFromBracket, FaGear } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const ProfileDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
+  const { logout } = useAuth();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -22,21 +27,57 @@ const ProfileDropdown = () => {
     };
   }, []);
 
-  const menus = ["Editar meu perfil", "Configurações", "Ajuda", "Sair"];
+  const navItems = [
+    {
+      icon: <FaRegUser />,
+      label: "Editar meu perfil",
+      path: "/profile",
+      method: "",
+    },
+    {
+      icon: <FaGear />,
+      label: "Configurações",
+      path: "/home/settings",
+      method: "",
+    },
+    { icon: <FaInfoCircle />, 
+      label: "Ajuda", 
+      path: "/home/help", 
+      method: "" },
+    {
+      icon: <FaArrowRightFromBracket />,
+      label: "Sair",
+      path: "/",
+      method: logout,
+    },
+  ];
 
   return (
     <div className="relative" ref={dropdownRef}>
-        <span onClick={toggleDropdown} className="mx-5 flex h-16 w-16 items-center justify-center rounded-full bg-primary text-primary-text dark:bg-primary cursor-pointer">GA</span>
+      <span
+        onClick={toggleDropdown}
+        className="mx-5 flex h-16 w-16 items-center justify-center rounded-full bg-primary text-primary-text dark:bg-primary cursor-pointer"
+      >
+        GA
+      </span>
 
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-80 bg-primary border border-border rounded-md shadow-lg z-10">
-            {menus.map((element, index) => (
-                <h3 key={index} className={`
-                text-center font py-2 text-lg cursor-pointer 
-                ${index < menus.length - 1 ? 'border-b hover:border-b-2' : ''}
+        <div className="absolute right-0 mt-2 w-60 bg-primary border border-border rounded-md shadow-lg z-10">
+          {navItems.map((element, index) => (
+            <Link
+              key={element.label}
+              to={element.path}
+              onClick={element.method}
+              className={`flex items-center justify-start py-2 gap-2 ml-2 text-lg cursor-pointer ${
+                index < navItems.length - 1 ? "border-b hover:border-b-2" : ""
+              }
                 hover:font-semibold
-              `}>{element}</h3>
-            ))}
+              `}
+            >
+              <span>{element.icon}</span>
+              {element.label}
+            </Link>
+          ))}
         </div>
       )}
     </div>
